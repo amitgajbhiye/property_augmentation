0cb27520e981b24d555c5e45735e3eb404ca27fd
amit gajbhiye <amitgajbhiye03@gmail.com>
2023-03-30 11:44:51 +0100

    bienc_bb_centp_chatgpt20k_pretrain

--- run23_bienc_bb_centp_chatgpt20k_pretrain.sh
+++ run23_bienc_bb_centp_chatgpt20k_pretrain.sh
@@ -9,12 +9,12 @@
 #SBATCH --ntasks=5
 #SBATCH --account scw1858
 
-#SBATCH --partition gpu,gpu_v100
+##SBATCH --partition dev
 
-#SBATCH --gres=gpu:1
+##SBATCH --gres=gpu:1
 #SBATCH --mem=10G
 
-#SBATCH --time 0-00:30:00
+#SBATCH --time 0-01:00:00
 
 conda activate venv
 
