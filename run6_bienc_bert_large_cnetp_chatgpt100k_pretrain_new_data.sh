6eec50f8011d65303d7989c6cec74e743bb7b225
amit gajbhiye <amitgajbhiye03@gmail.com>
2023-05-09 18:40:11 +0100

    cnetp+chatgpt 100K bb and bl pretrain

---
+++ run6_bienc_bert_large_cnetp_chatgpt100k_pretrain_new_data.sh
@@ -0,0 +1,22 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=blCnetPChatGptPretrain
+
+#SBATCH --output=logs/cnetp_chatgpt_pretrain/out_bienc_bert_large_cnetp_chatgpt100k_pretrain_new_data.txt
+#SBATCH --error=logs/cnetp_chatgpt_pretrain/err_bienc_bert_large_cnetp_chatgpt100k_pretrain_new_data.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH --account scw1858
+
+#SBATCH --partition gpu_v100
+#SBATCH --gres=gpu:1
+#SBATCH --mem=16G
+
+#SBATCH --time 2-00:00:00
+
+conda activate venv
+
+python3 bienc_run_model.py --config_file configs/1_configs_pretrain/bienc_bert_large_cnetp_chatgpt100k_pretrain_new_data.json
+
+echo 'Job Finished!'
