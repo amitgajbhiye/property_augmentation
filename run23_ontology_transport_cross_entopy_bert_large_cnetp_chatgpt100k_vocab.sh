b265be8eb63e1ed0da5608ec7d0fbeffbf8a3d0c
amit gajbhiye <amitgajbhiye03@gmail.com>
2023-06-14 18:36:12 +0100

    clusters ontology wine transport

---
+++ run23_ontology_transport_cross_entopy_bert_large_cnetp_chatgpt100k_vocab.sh
@@ -0,0 +1,34 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=transport_cnetp_chatgpt100k_vocab_and_model
+
+#SBATCH --output=logs/ontology_completion/transport/out_cross_entopy_bert_large_cnetp_chatgpt100k_vocab_and_model.txt
+#SBATCH --error=logs/ontology_completion/transport/err_cross_entopy_bert_large_cnetp_chatgpt100k_vocab_and_model.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH -A scw1858
+
+#SBATCH -p gpu_v100,gpu
+#SBATCH --gres=gpu:1
+
+#SBATCH --mem=16G
+#SBATCH -t 1-00:00:00
+
+conda activate venv
+
+python3 get_embeds_and_train_data.py --config_file configs/ontology_completion_cnetpchatgpt100k_vocab_and_model/transport/1_transport_con_embed.json
+python3 get_embeds_and_train_data.py --config_file configs/ontology_completion_cnetpchatgpt100k_vocab_and_model/transport/2_cnetp_chatgpt100k_vocab_embeds.json
+python3 get_embeds_and_train_data.py --config_file configs/ontology_completion_cnetpchatgpt100k_vocab_and_model/transport/3_con_similar_50cnetp_chatgpt100k_props.json
+
+python3 je_filter_similar_props.py --config_file configs/ontology_completion_cnetpchatgpt100k_vocab_and_model/transport/4_mcrae_cslb_deb3l_filter_con_sim_50props_filterthresh50_75_90.json
+
+python3 get_property_similar_concepts.py --config_file configs/ontology_completion_cnetpchatgpt100k_vocab_and_model/transport/5_get_prop_similar50cons_filterthresh50.json
+python3 get_property_similar_concepts.py --config_file configs/ontology_completion_cnetpchatgpt100k_vocab_and_model/transport/5_get_prop_similar50cons_filterthresh75.json
+python3 get_property_similar_concepts.py --config_file configs/ontology_completion_cnetpchatgpt100k_vocab_and_model/transport/5_get_prop_similar50cons_filterthresh90.json
+
+python3 je_filter_similar_props.py --config_file configs/ontology_completion_cnetpchatgpt100k_vocab_and_model/transport/6_mcrae_cslb_deb3l_filter_prop_sim_50cons_filterthresh50.json
+python3 je_filter_similar_props.py --config_file configs/ontology_completion_cnetpchatgpt100k_vocab_and_model/transport/6_mcrae_cslb_deb3l_filter_prop_sim_50cons_filterthresh75.json
+python3 je_filter_similar_props.py --config_file configs/ontology_completion_cnetpchatgpt100k_vocab_and_model/transport/6_mcrae_cslb_deb3l_filter_prop_sim_50cons_filterthresh90.json
+
+echo 'Job Finished !!!'
---
+++ run23_ontology_wine_cross_entopy_bert_large_cnetp_chatgpt100k_vocab.sh
@@ -0,0 +1,34 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=wine_cnetp_chatgpt100k_vocab_and_model
+
+#SBATCH --output=logs/ontology_completion/wine/out_cross_entopy_bert_large_cnetp_chatgpt100k_vocab_and_model.txt
+#SBATCH --error=logs/ontology_completion/wine/err_cross_entopy_bert_large_cnetp_chatgpt100k_vocab_and_model.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH -A scw1858
+
+#SBATCH -p gpu_v100,gpu
+#SBATCH --gres=gpu:1
+
+#SBATCH --mem=16G
+#SBATCH -t 1-00:00:00
+
+conda activate venv
+
+python3 get_embeds_and_train_data.py --config_file configs/ontology_completion_cnetpchatgpt100k_vocab_and_model/wine/1_wine_con_embed.json
+python3 get_embeds_and_train_data.py --config_file configs/ontology_completion_cnetpchatgpt100k_vocab_and_model/wine/2_cnetp_chatgpt100k_vocab_embeds.json
+python3 get_embeds_and_train_data.py --config_file configs/ontology_completion_cnetpchatgpt100k_vocab_and_model/wine/3_con_similar_50cnetp_chatgpt100k_props.json
+
+python3 je_filter_similar_props.py --config_file configs/ontology_completion_cnetpchatgpt100k_vocab_and_model/wine/4_mcrae_cslb_deb3l_filter_con_sim_50props_filterthresh50_75_90.json
+
+python3 get_property_similar_concepts.py --config_file configs/ontology_completion_cnetpchatgpt100k_vocab_and_model/wine/5_get_prop_similar50cons_filterthresh50.json
+python3 get_property_similar_concepts.py --config_file configs/ontology_completion_cnetpchatgpt100k_vocab_and_model/wine/5_get_prop_similar50cons_filterthresh75.json
+python3 get_property_similar_concepts.py --config_file configs/ontology_completion_cnetpchatgpt100k_vocab_and_model/wine/5_get_prop_similar50cons_filterthresh90.json
+
+python3 je_filter_similar_props.py --config_file configs/ontology_completion_cnetpchatgpt100k_vocab_and_model/wine/6_mcrae_cslb_deb3l_filter_prop_sim_50cons_filterthresh50.json
+python3 je_filter_similar_props.py --config_file configs/ontology_completion_cnetpchatgpt100k_vocab_and_model/wine/6_mcrae_cslb_deb3l_filter_prop_sim_50cons_filterthresh75.json
+python3 je_filter_similar_props.py --config_file configs/ontology_completion_cnetpchatgpt100k_vocab_and_model/wine/6_mcrae_cslb_deb3l_filter_prop_sim_50cons_filterthresh90.json
+
+echo 'Job Finished !!!'
