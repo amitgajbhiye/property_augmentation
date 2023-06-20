f0a4c8799a6606756407859fd332d7700f1b86c5
amit gajbhiye <amitgajbhiye03@gmail.com>
2023-06-20 13:38:18 +0100

    mcrae prop similar analysis

--- run32_mcrae_analysis_prop_sim_concepts.sh
+++ run32_mcrae_analysis_prop_sim_concepts.sh
@@ -22,6 +22,6 @@ python3 get_embeds_and_train_data.py --config_file configs/mcrae_analysis_exp/pr
 python3 get_embeds_and_train_data.py --config_file configs/mcrae_analysis_exp/prop_similar_analysis/2_mcrae_prop_vocab_embeds.json
 python3 get_property_similar_concepts.py --config_file configs/mcrae_analysis_exp/prop_similar_analysis/3_get_prop_similar50cons.json
 
-python3 get_embeds_and_train_data.py --config_file configs/2_configs_get_embeds_and_train_data/ufet/cnetp_chatgpt100k_vocab_chatgpt100k_bienc/3_ufet_type_similar_50cnetp_chatgpt100k_props.json
+python3 je_filter_similar_props.py --config_file configs/mcrae_analysis_exp/prop_similar_analysis/4_mcrae_cslb_deb3l_filter_con_sim_50props_filterthresh50_75_90.json
 
 echo 'Job Finished !!!'
