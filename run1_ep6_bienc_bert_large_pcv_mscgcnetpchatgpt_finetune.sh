c8a7e3806776d1c2897a589c9f4eeb4e97ac5551
amit gajbhiye <amitgajbhiye03@gmail.com>
2023-08-31 12:28:48 +0100

    bert large mscg cnetp chatgpt data property split finetune

---
+++ run1_ep6_bienc_bert_large_pcv_mscgcnetpchatgpt_finetune.sh
@@ -0,0 +1,22 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=blMCCEP6
+
+#SBATCH --output=logs/chatgpt_finetune/out_bert_large_mcrae_pcv_mscgchatgpt100k_finetune_6epoch.txt
+#SBATCH --error=logs/chatgpt_finetune/err_bert_large_mcrae_pcv_mscgchatgpt100k_finetune_6epoch.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH -A scw1858
+
+#SBATCH -p gpu_v100
+#SBATCH --mem=16g
+#SBATCH --gres=gpu:1
+
+#SBATCH -t 0-8:00:00
+
+conda activate venv
+
+python3 bienc_fine_tune.py --config_file configs/3_finetune/bert_large_mcrae_pcv_mscgchatgpt100k_finetune_6epoch_config.json
+
+echo 'Job Finished !!!'
