3ad32b5e4939ec999915965f260be2987540d1f8
amit gajbhiye <amitgajbhiye03@gmail.com>
2024-06-12 14:54:45 +0100

    biencoders concept facet property

---
+++ run7_pretrain_bienc_bert_large_llama3_transport_onto_concept_property_facet.sh
@@ -0,0 +1,22 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=cpf
+
+#SBATCH --output=logs/llama3_bienc/out_pretrain_bienc_bert_large_llama3_transport_onto_concept_property_facet.txt
+#SBATCH --error=logs/llama3_bienc/err_pretrain_bienc_bert_large_llama3_transport_onto_concept_property_facet.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH --account scw1858
+
+#SBATCH --partition gpu_v100,gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=16G
+
+#SBATCH --time 2-00:00:00
+
+conda activate venv
+
+python3 bienc_run_model.py --config_file configs/commonality_llama/pretrain_bienc_bert_large_llama3_transport_onto_concept_property_facet.json
+
+echo 'Job Finished!'
